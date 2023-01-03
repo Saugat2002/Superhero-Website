c9bc2b159d872f5720bdb2f8ae43605e4f2f9bb0
Saugat Adhikari <102962967+Saugat2002@users.noreply.github.com>
2023-01-03 11:39:03 +0545

    commit 3

--- index.js
+++ index.js
@@ -3,40 +3,73 @@ const heroDescriptionDiv = document.getElementById("heroDescription");
 const searchInputDiv = document.getElementById("searchInput");
 const searchBtn = document.getElementById("search");
 let superheroId;
-var delayInMilliseconds = 3000;
+var delayInMilliseconds = 1000;
+
+const showLoading = () => {
+  heroDescriptionDiv.innerHTML = `
+  <div class="flex flex-col items-center justify-evenly h-56">
+  <span class="font-mono text-3xl ">LOADING</span>
+  <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+  </div>
+  `;
+  setTimeout(function () {
+    heroDescriptionDiv.innerHTML = "";
+  }, delayInMilliseconds);
+};
 
 const showHeroInfo = (json) => {
-  heroDescriptionDiv.innerHTML = `
-        <div class="flex flex-col items-center justify-evenly h-56">
-            <span class="font-mono text-3xl ">LOADING</span>
-            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </div>
-        `;
+  let name
   setTimeout(function () {
-    heroDescriptionDiv.innerHTML = `
-        <div class="flex flex-col items-center">
-            <span class="font-sans font-bold pb-5 text-3xl">${json.name}</span>
-            <img src="${json.image.url}" alt="heroImage" width="200" class="rounded-xl"/>
+    heroDescriptionDiv.innerHTML += `
+    <div class="flex justify-start items-start space-x-10 ml-5">
+        <div class="flex flex-col ">
+            <span class="font-sans font-bold pb-5 text-3xl hover:text-yellow-500 hover:-translate-y-1 hover:scale-105  transition ease-in-out duration-300">${json.name}</span>
+            <img src="${json.image.url}" alt="heroImage"class="w-60 rounded-xl hover:-translate-y-1 hover:scale-105  transition ease-in-out duration-300"/>
         </div>
-        <div class="stats font-mono flex flex-col space-y-7 mt-10">
-            <span> 🧠 INTELLIGENCE : ${json.powerstats.intelligence} </span>
-            <span> 💪 STRENGTH : ${json.powerstats.strength}</span>
-            <span> ⚡ SPEED : ${json.powerstats.speed}</span>
-            <span>🏋️‍♂️ DURABILITY : ${json.powerstats.durability}</span>
-            <span>📊 POWER : ${json.powerstats.power}</span>
-            <span>⚔️ COMBAT : ${json.powerstats.combat}</span>
+        <div class="stats font-mono flex space-x-7 mt-10">
+            <ul class="list-disc p-4 space-y-3">
+              <span class="text-2xl cursor-pointer underline hover:text-yellow-500">POWERSATS</span>
+              <li> 🧠 INTELLIGENCE : ${json.powerstats.intelligence} </li>
+              <li> 💪 STRENGTH : ${json.powerstats.strength}</li>
+              <li> ⚡ SPEED : ${json.powerstats.speed}</li> 
+              <li>🏋️‍♂️ DURABILITY : ${json.powerstats.durability}</li>
+              <li>📊 POWER : ${json.powerstats.power}</li>
+              <li>⚔️ COMBAT : ${json.powerstats.combat}</li>
+            </ul>
+            <ul class="list-disc p-4 space-y-3">
+            <span class="text-2xl cursor-pointer underline hover:text-yellow-500">BIOGRAPHY</span>
+            <li>Full Name: ${Object.values(json.biography)[0]}</li>
+            <li>Alter-Egos: ${Object.values(json.biography)[1]}</li>
+            <li>Aliases: ${Object.values(json.biography)[2]}</li>
+            <li>Place of Birth: ${Object.values(json.biography)[3]}</li>
+            <li>First Appearance: ${Object.values(json.biography)[4]}</li>
+            <li>Publisher: ${Object.values(json.biography)[5]}</li>
+            </ul>
+            <ul class="list-disc p-4 space-y-3">
+            <span class="text-2xl cursor-pointer underline hover:text-yellow-500">APPEARANCE</span>
+              <li>Gender: ${json.appearance.gender}</li>
+              <li>Race: ${json.appearance.race}</li>
+              <li>Height: ${json.appearance.height}</li>
+              <li>Weight: ${json.appearance.weight}</li>
+              <li>Eye Color: ${Object.values(json.appearance)[4]}</li>
+              <li>Hair Color: ${Object.values(json.appearance)[5]}</li>
+              </ul>
+          </span> 
         </div>
+        </div>
+        <hr class="border-gray-700 ml-10">
                 `;
   }, delayInMilliseconds);
 };
+
 const getSuperHero = () => {
   superheroId = Math.ceil(Math.random() * 731);
   fetch(`https://superheroapi.com/api.php/10223569763528853/${superheroId}`)
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
-      const superHero = json;
-      showHeroInfo(superHero);
+      showLoading();
+      showHeroInfo(json);
     });
 };
 
@@ -46,15 +79,76 @@ const getSuperHeroSearch = (str) => {
   fetch(`https://superheroapi.com/api.php/10223569763528853/search/${name}`)
     .then((response) => response.json())
     .then((json) => {
-        console.log(json);
-      const superHero = json.results[0];
-      showHeroInfo(superHero);
+      console.log(json);
+      if (json) {
+        showLoading();
+        for (const superHero of json.results) {
+          showHeroInfo(superHero);
+        }
+      }
     });
 };
 
-searchBtn.onclick = () => {
-    getSuperHeroSearch(searchInputDiv.value);
-}
-randomHeroBtn.onclick = () => {
-  getSuperHero();
+const getDescription = (json) => {
+  descriptionOtherDiv.innerHTML = `
+    <div class="left flex flex-col">
+    <h1
+    class=" flex items-center justify-center text-center text-5xl font-mono font-bold text-cyan-600 my-10   hover:text-slate-400 cursor-pointer hover:scale-105 hover:-translate-y-1 transition ease-in-out     duration-300">${json.name}
+    </h1>
+    <a href="${json.img.url}" target="_blank"
+    class="transform motion-reduce:transform-none hover:-translate-y-1 hover:scale-105 transition ease-in-out duration-300">
+    <img src="${json.img.url}" alt="" width="350"
+    class="rounded-lg hover:ring-2 ring-gray ring-solid" />
+    </a>
+    </div>
+    <div class="text-xl details flex flex-col w-fit space-y-10 mt-36">
+    <span class="biography flex flex-col space-x-10 hover:[&>span:nth-child(1)]:text-yellow-500">
+    <span class="text-2xl cursor-pointer underline">BIOGRAPHY</span>
+    <ul class="list-disc p-4">
+    <li>Full Name: </li>
+    <li>Alter-Egos: </li>
+    <li>Aliases: </li>
+    <li>Place of Birth: </li>
+    <li>First Appearance: </li>
+    <li>Publisher: </li>
+    </ul>
+    </span>
+    <span class="powerstats flex flex-col space-x-10 hover:[&>span:nth-child(1)]:text-yellow-500">
+    <span class="text-2xl cursor-pointer underline">POWERSTATS</span>
+    <ul class="list-disc p-4">
+    <li>Intelligence: </li>
+    <li>Strength: </li>
+    <li>Speed: </li>
+    <li>Durability: </li>
+    <li>Power: </li>
+    <li>Combat: </li>
+    </ul>
+    </span>
+    <span class="appearance flex flex-col space-x-10 hover:[&>span:nth-child(1)]:text-yellow-500 ">
+    <span class="text-2xl cursor-pointer underline">APPEARANCE</span>
+    <ul class="list-disc p-4">
+    <li>Gender: </li>
+    <li>Race: </li>
+    <li>Height: </li>
+    <li>Weight: </li>
+    <li>Eye Color: </li>
+    <li>Hair Color: </li>
+    </ul>
+    </span>
+    <span class="work flex flex-col space-x-10 hover:[&>span:nth-child(1)]:text-yellow-500 ">
+    <span class="text-2xl cursor-pointer underline">WORK</span>
+    <ul class="list-disc p-4">
+    <li>Occupation: </li>
+    <li>Base: </li>
+    </ul>
+    </span>
+    <span class="connections flex flex-col space-x-10 hover:[&>span:nth-child(1)]:text-yellow-500">
+    <span class="text-2xl cursor-pointer underline">CONNECTIONS</span>
+    <ul class="list-disc p-4">
+    <li>Group Affiliations: </li>
+    <li>Base: </li>
+    </ul>
+    </span>
+    </div>
+    `;
 };
